b328f78b2b41d727a2bb2e91f2aaddf3e80e97b6
Brhutcraft <brhutcraft@gmail.com>
2025-05-05 15:46:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() { // Sửa 'Document' thành 'document'
 
     // --- Menu Toggle Logic ---
     const navLinks = document.getElementById('navLinks');
